761d2d7ba1a98452c78b1d0b68f8fc9dd5b10892
MAYUR BIJARNIYA <83810766+mayurbijarniya@users.noreply.github.com>
2024-04-25 16:43:50 +0530

    DocuCraft

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Editor.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Editor.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Doc> Docs { get; set; }
+    }
+}
